ac0d0b592088a8b2b028a28c9fe877fbb95d8d12
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2022-01-10 20:43:47 +0900

    fontawesome をセットアップする

--- apps/room-booking/src/app/app.module.ts
+++ apps/room-booking/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
